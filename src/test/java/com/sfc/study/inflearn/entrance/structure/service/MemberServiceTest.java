cf79f072306becc6fdf8f00741c96f7fdc458209
chaeyjs827 <chaeyjs827@gmail.com>
2024-04-14 20:12:28 +0900

    [인프런 학습] 1. 회원 서비스 테스트

---
+++ src/test/java/com/sfc/study/inflearn/entrance/structure/service/MemberServiceTest.java
@@ -0,0 +1,63 @@
+package com.sfc.study.inflearn.entrance.structure.service;
+
+import com.sfc.study.inflearn.entrance.structure.repository.structure.domain.Member;
+import com.sfc.study.inflearn.entrance.structure.repository.structure.repository.MemoryMemberRepository;
+import com.sfc.study.inflearn.entrance.structure.repository.structure.service.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+    MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        memoryMemberRepository.clearStore();
+    }
+
+    @Test
+    void test_join() {
+        // given
+        Member member = new Member();
+        member.setName("Hello");
+
+        Long saveId = memberService.join(member);
+
+
+        // when
+        // then
+        Member foundMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(foundMember.getName());
+    }
+
+
+    @Test
+    public void duplicatedMemberExceptino() {
+        // given
+        Member member = new Member();
+        member.setName("spring1");
+
+        Member member2 = new Member();
+        member.setName("spring1");
+
+        // when
+        memberService.join(member);
+        org.junit.jupiter.api.Assertions.assertThrows(
+                IllegalStateException.class, () -> memberService.join(member2)
+        );
+
+        // then
+
+
+    }
+
+    @Test
+    void test_find_members() {
+    }
+
+    @Test
+    void test_find_one() {
+    }
+}
