22e70739cf46b5e1ecc413e6f582e58a939de3fa
chaeyjs827 <chaeyjs827@gmail.com>
2024-06-07 21:01:42 +0900

    [스프링 학습] 1. redis 연동 2. redis bench marking * 하지만 명확한 벤치 마킹 원인은 모르겠습니다. 부하 테스트를해서 redis cluster나 샤딩을 고려하면 된다곤 하지만.. 테스트 데이터가 적어서 그런지 아직 벤치 마킹의 중요성은 모르겠습니다.

---
+++ src/main/java/com/sfc/study/redis/benchmark/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.sfc.study.redis.benchmark.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+// todo Redis 벤치 마킹 테스트 용으로 잠시 사용
+//@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+}
