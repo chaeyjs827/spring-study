2d2b7cf7ba019580acc8ee651224dfef2b35d210
chaeyjs827 <chaeyjs827@gmail.com>
2024-06-08 22:54:50 +0900

    [스프링 학습] 1. 리팩토링

--- src/main/java/com/sfc/study/redis/benchmark/config/RedisConfig.java
+++ src/main/java/com/sfc/study/redis/benchmark/config/RedisConfig.java
@@ -1,7 +1,6 @@
 package com.sfc.study.redis.benchmark.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
