5ad89777509afb804d4b861306fff6ba0780fced
chaeyjs827 <chaeyjs827@gmail.com>
2024-06-29 00:24:21 +0900

    [Kafka] 1. Kafka 테스트 * Producer * Consumer -> Kafka 포트 = 9094
    => Producer 메세지 발행 시 Consumer 수신

---
+++ src/main/java/com/sfc/study/kafka/listener/KafkaConsumerListener.java
@@ -0,0 +1,14 @@
+package com.sfc.study.kafka.listener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerListener {
+
+    @KafkaListener(topics = "test-topic", groupId = "test-group")
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
+    }
+
+}
